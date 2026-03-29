db030d4f58c48b0d403e2e0fc4cc61ab5677de0f
Goodz_sun <poggermarble@gmail.com>
2026-03-29 17:22:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-// show your website
-app.use(express.static(__dirname));
+// Serve your static files (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname)));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(PORT, () => {
-  console.log("Server running");
+  console.log(`Server running on port ${PORT}`);
 });
